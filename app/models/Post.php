3ad53de65ba782fa642ecd2d0de435825ba77793
Darin <dmcfarlane@fordham.edu>
2024-03-12 19:53:19 -0400

    Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,5 +4,25 @@ namespace app\models;
 
 class Post
 {
+    public function getAllPosts() {
+        return [
+            [
+                'id' => 1,
+                'title' => 'First Post',
+                'content' => 'Content for the first post'
+            ],
+            
+            [
+                'id' => 2,
+                'title' => 'Second Post',
+                'content' => 'Content for the second post'
+            ],
 
+            [   'id'=> 3,
+                'title' => 'Third Post',
+                'content'=> 'Content for the third post'
+            ], 
+            // Will add more posts as go along
+        ];
+    }
 }
