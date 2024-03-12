661985434a5d9d4e48e0d41afe1ec169a4cef498
Darin <dmcfarlane@fordham.edu>
2024-03-12 19:52:31 -0400

    Adding Main and Post php files

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 use app\core\Controller;
 use app\models\Post;
+use app\models\User;
 
 class MainController extends Controller
 {
@@ -21,6 +22,12 @@ class MainController extends Controller
         //todo create a 404 twig template in app/public/assets/views
         //an example is in app/controllers/UsersController
         //and return it from this method
+
+        // Load the 404 Twig template
+        $template = $this->twig->load('errors/404.twig');
+        
+        // Render and return the 404 page
+        echo $template->render();
     }
 
 }
