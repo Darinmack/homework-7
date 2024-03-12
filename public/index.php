bc9fab2eb979cb8d40ab44bd3128aa3ba728f835
Darin <dmcfarlane@fordham.edu>
2024-03-12 19:55:53 -0400

    index.php

--- public/index.php
+++ public/index.php
@@ -13,7 +13,30 @@ use app\controllers\PostController;
 
 $url = $_SERVER["REQUEST_URI"];
 
+$mainController = new MainController();
+$postController = new PostController();
+   
+    switch ($url) {
+
+        case '/':
+          
+            $mainController->homepage();
+            break;
+
+        case '/posts':
+       
+            $postController->posts();
+            break;
+
+       
+        default:
+            $mainController->notFound();
+            break;
+    }
+
+
 //todo add a switch statement router to route based on the url
 //if it is "/posts" return an array of posts via the post controller
 //if it is "/" return the homepage view from the main controller
 //if it is something else return a 404 view from the main controller
+
